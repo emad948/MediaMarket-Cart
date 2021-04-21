df9bb0bfa89fb8cdccecfe53aa9e161a5bb618b7
Emad <almsouti.e@gmail.com>
2021-04-21 08:06:16 +0200

    Updates

---
+++ precache-manifest.1c124c5231b089f5407d0f8267b91883.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04578abbc9d8e6715c8144aeed192751",
+    "url": "/MediaMarket-Cart/index.html"
+  },
+  {
+    "revision": "683c0e2c59459539b9dd",
+    "url": "/MediaMarket-Cart/static/css/main.be986e48.chunk.css"
+  },
+  {
+    "revision": "ea433fadd362cad827a8",
+    "url": "/MediaMarket-Cart/static/js/2.1d915460.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/MediaMarket-Cart/static/js/2.1d915460.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "683c0e2c59459539b9dd",
+    "url": "/MediaMarket-Cart/static/js/main.9e3324cf.chunk.js"
+  },
+  {
+    "revision": "6f7975fdc3618586dd41",
+    "url": "/MediaMarket-Cart/static/js/runtime-main.a0687630.js"
+  }
+]);
